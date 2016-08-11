ed50d5a43e9c4efb5b71723fceeaa7307f0c3fe2
nataliehess <hess.natalie@yahoo.com>
2016-08-11 06:48:34 -0500

    Update washerf.py

--- Clients/washerf.py
+++ Clients/washerf.py
@@ -1,5 +1,5 @@
-#This Code represents Oven which connects to an MQTT Broker which gets and sets device attributes 
-#Vamshi Teja
+#This Code represents Refrigerator which connects to an MQTT Broker which gets and sets device attributes 
+#Vamshi Teja and Natalie Hess
 
 import paho.mqtt.client as paho
 import RPi.GPIO as GPIO
@@ -17,19 +17,19 @@ buttonPin    = 7
 ledPin       = 12
 door_stat    = 21
 cur_temp     = 23
-cur_state    = 29
+cur_spill    = 29
 temp_set     = 31
 door_set     = 33
-preheat_set  = 8
+spill_set  = 8
 
 GPIO.setmode(GPIO.BOARD)          # use P1 header pin numbering convention
 GPIO.cleanup()                    # clean up resources
 GPIO.setup(door_stat, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(cur_temp, GPIO.IN)
-GPIO.setup(cur_state, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(cur_spill, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(temp_set, GPIO.OUT)
 GPIO.setup(door_set, GPIO.OUT)
-GPIO.setup(preheat_set,  GPIO.OUT)
+GPIO.setup(spill_set,  GPIO.OUT)
 
 #to read analog values
 def RC_Analog (Pin):  
@@ -61,16 +61,16 @@ def on_message(client, obj, msg):
     json_data = msg.payload
     # check the status property value
     print(json_data)
-    door_set = json.loads(json_data)['oven'][0]['door_set']
-    temp_set = json.loads(json_data)['oven'][1]['temp_set']
-    preheat_set = json.loads(json_data)['oven'][0]['preheat_set']
+    door_set = json.loads(json_data)['refrigerator'][0]['door_set']
+    temp_set = json.loads(json_data)['refrigerator'][1]['temp_set']
+    spill_set = json.loads(json_data)['refrigerator'][0]['spill_set']
     if door_set == 'on':
         door_set = GPIO.HIGH
         GPIO.output(door_set, GPIO.HIGH)
     elif door_set == 'off':
         led_status = GPIO.LOW
         GPIO.output(ledPin, GPIO.LOW)
-    if preheat_set == 'on':
+    if spill_set == 'on':
         door_set = GPIO.HIGH
         GPIO.output(door_set, GPIO.HIGH)
     elif(door_set == 'off'):
@@ -96,12 +96,12 @@ client.on_subscribe = on_subscribe
 
 
 # device topics
-in_oven_preheat_set  = 'oven/preheat_set' + device_id + '/get'   # receiving messages
-in_oven_temp_set     = 'oven/temp_set' + device_id + '/get'      # receiving messages
-in_oven_door_set     = 'oven/door_set' + device_id + '/get'      # receiving messages
-out_oven_door_stat   = 'oven/door_stat' + device_id + '/set'     # publishing messages
-out_oven_cur_stat    = 'oven/cur_stat' + device_id + '/set'      # publishing messages
-out_oven_cur_temp    = 'oven/cur_temp' + device_id + '/set'      # publishing messages
+in_refrigerator_spill_set  = 'refrigerator/spill_set' + device_id + '/get'   # receiving messages
+in_refrigerator_temp_set     = 'refrigerator/temp_set' + device_id + '/get'      # receiving messages
+in_refrigerator_door_set     = 'refrigerator/door_set' + device_id + '/get'      # receiving messages
+out_refrigerator_door_stat   = 'refrigerator/door_stat' + device_id + '/set'     # publishing messages
+out_refrigerator_cur_spill    = 'refrigerator/cur_spill' + device_id + '/set'      # publishing messages
+out_refrigerator_cur_temp    = 'refrigerator/cur_temp' + device_id + '/set'      # publishing messages
 
 
 # client connection
@@ -113,8 +113,8 @@ client.subscribe('oven/#/get', 1)                     # MQTT subscribtion (with 
 # Button logic 
 prev_door_stat        = GPIO.LOW
 led_status_door_stat  = GPIO.LOW
-prev_cur_stat         = GPIO.LOW
-led_status_cur_stat   = GPIO.LOW
+prev_cur_spill         = GPIO.LOW
+led_status_cur_spill   = GPIO.LOW
 updated_at  = 0  # the last time the output pin was toggled
 debounce    = 0.5  # the debounce time, increase if the output flickers
 
@@ -123,7 +123,7 @@ rc = 0
 while rc == 0:
     rc = client.loop()
     door_status = GPIO.input(door_stat)
-    cur_status  = GPIO.input(cur_stat)
+    cur_status  = GPIO.input(cur_spill)
     cur_temp    = RC_Analog(cur_temp)
     if door_status != prev_door_stat and time.time() - updated_at > debounce:
         prev_door_stat = door_status
@@ -137,21 +137,21 @@ while rc == 0:
             # effectively update the light status
             GPIO.output(door_stat, door_status)
             payload_door = { 'oven': [{ 'id': '518be5a700045e1521000001', 'door_status': payload_door }] }
-    if cur_status != prev_cur_stat and time.time() - updated_at > debounce:
-        prev_cur_stat = cur_status
+    if cur_status != prev_cur_spill and time.time() - updated_at > debounce:
+        prev_cur_spill = cur_status
         updated_at  = time.time()
 
         if cur_status:
             cur_status = not led_status_door_stat
-            cur_stat_payload = 'off'
-            if led_status_cur_stat == GPIO.HIGH:
-                cur_stat_payload = 'on'
+            cur_spill_payload = 'off'
+            if led_status_cur_spill == GPIO.HIGH:
+                cur_spill_payload = 'on'
             # effectively update the light status
             GPIO.output(door_stat, door_status)
-            payload_cur = { 'oven': [{ 'id': '', 'cur_status': payload_cur }] }
-            payload_temp = { 'oven': [{ 'id': '', 'cur_temp': cur_temp }] }
-client.publish(out_oven_door_stat, json.dumps(payload_door))
-client.publish(out_oven_cur_stat, json.dumps(payload_cur))
-client.publish(out_oven_cur_temp, json.dumps(payload_temp))
+            payload_cur = { 'refrigerator': [{ 'id': '', 'cur_status': payload_cur }] }
+            payload_temp = { 'refrigerator': [{ 'id': '', 'cur_temp': cur_temp }] }
+client.publish(out_refrigerator_door_stat, json.dumps(payload_door))
+client.publish(out_refrigerator_cur_spill, json.dumps(payload_cur))
+client.publish(out_refrigerator_cur_temp, json.dumps(payload_temp))
 
 print('rc: ' + str(rc)) 
