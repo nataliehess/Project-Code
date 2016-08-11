a0d7c595c8e6a338ec0e32098dd7a467d186d18f
rajeshvaragani <ee15btech11035@iith.ac.in>
2016-08-12 02:40:56 +0530

    edited as required for washer

--- Clients/washerf.py
+++ Clients/washerf.py
@@ -14,22 +14,25 @@ random_client_id = '<CLIENT_ID>'      #  set a random client_id (max 23 char)
 # Board settings 
 
 buttonPin    = 7
-ledPin       = 12
+ledsoak       = 12
+ledwash       =13
+ledrinse     =14
+ledspin      =15
 door_stat    = 21
-cur_temp     = 23
+#cur_temp     = 23
 cur_state    = 29
-temp_set     = 31
+#temp_set     = 31
 door_set     = 33
-preheat_set  = 8
+#preheat_set  = 8
 
 GPIO.setmode(GPIO.BOARD)          # use P1 header pin numbering convention
 GPIO.cleanup()                    # clean up resources
 GPIO.setup(door_stat, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(cur_temp, GPIO.IN)
+#GPIO.setup(cur_temp, GPIO.IN)
 GPIO.setup(cur_state, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(temp_set, GPIO.OUT)
+#GPIO.setup(temp_set, GPIO.OUT)
 GPIO.setup(door_set, GPIO.OUT)
-GPIO.setup(preheat_set,  GPIO.OUT)
+#GPIO.setup(preheat_set,  GPIO.OUT)
 
 #to read analog values
 def RC_Analog (Pin):  
@@ -61,9 +64,9 @@ def on_message(client, obj, msg):
     json_data = msg.payload
     # check the status property value
     print(json_data)
-    door_set = json.loads(json_data)['oven'][0]['door_set']
-    temp_set = json.loads(json_data)['oven'][1]['temp_set']
-    preheat_set = json.loads(json_data)['oven'][0]['preheat_set']
+    door_set = json.loads(json_data)['washer'][0]['door_set']
+    temp_set = json.loads(json_data)['washer'][1]['temp_set']
+    preheat_set = json.loads(json_data)['washer'][0]['preheat_set']
     if door_set == 'on':
         door_set = GPIO.HIGH
         GPIO.output(door_set, GPIO.HIGH)
@@ -96,18 +99,18 @@ client.on_subscribe = on_subscribe
 
 
 # device topics
-in_oven_preheat_set  = 'oven/preheat_set' + device_id + '/get'   # receiving messages
-in_oven_temp_set     = 'oven/temp_set' + device_id + '/get'      # receiving messages
-in_oven_door_set     = 'oven/door_set' + device_id + '/get'      # receiving messages
-out_oven_door_stat   = 'oven/door_stat' + device_id + '/set'     # publishing messages
-out_oven_cur_stat    = 'oven/cur_stat' + device_id + '/set'      # publishing messages
-out_oven_cur_temp    = 'oven/cur_temp' + device_id + '/set'      # publishing messages
+in_oven_preheat_set  = 'washer/preheat_set' + device_id + '/get'   # receiving messages
+in_oven_temp_set     = 'washer/temp_set' + device_id + '/get'      # receiving messages
+in_oven_door_set     = 'washer/door_set' + device_id + '/get'      # receiving messages
+out_oven_door_stat   = 'washer/door_stat' + device_id + '/set'     # publishing messages
+out_oven_cur_stat    = 'washer/cur_stat' + device_id + '/set'      # publishing messages
+out_oven_cur_temp    = 'washer/cur_temp' + device_id + '/set'      # publishing messages
 
 
 # client connection
 client.username_pw_set(device_id, device_secret)      # MQTT server credentials
 client.connect("127.0.0.1",1883,60)                   # MQTT server address
-client.subscribe('oven/#/get', 1)                     # MQTT subscribtion (with QoS level 0)
+client.subscribe('washer/#/get', 1)                     # MQTT subscribtion (with QoS level 0)
 
 
 # Button logic 
@@ -124,7 +127,7 @@ while rc == 0:
     rc = client.loop()
     door_status = GPIO.input(door_stat)
     cur_status  = GPIO.input(cur_stat)
-    cur_temp    = RC_Analog(cur_temp)
+   # cur_temp    = RC_Analog(cur_temp)
     if door_status != prev_door_stat and time.time() - updated_at > debounce:
         prev_door_stat = door_status
         updated_at  = time.time()
@@ -136,7 +139,7 @@ while rc == 0:
                 door_stat_payload = 'on'
             # effectively update the light status
             GPIO.output(door_stat, door_status)
-            payload_door = { 'oven': [{ 'id': '518be5a700045e1521000001', 'door_status': payload_door }] }
+            payload_door = { 'washer': [{ 'id': '518be5a700045e1521000001', 'door_status': payload_door }] }
     if cur_status != prev_cur_stat and time.time() - updated_at > debounce:
         prev_cur_stat = cur_status
         updated_at  = time.time()
@@ -148,10 +151,10 @@ while rc == 0:
                 cur_stat_payload = 'on'
             # effectively update the light status
             GPIO.output(door_stat, door_status)
-            payload_cur = { 'oven': [{ 'id': '', 'cur_status': payload_cur }] }
-            payload_temp = { 'oven': [{ 'id': '', 'cur_temp': cur_temp }] }
+            payload_cur = { 'washer': [{ 'id': '', 'cur_status': payload_cur }] }
+            #payload_temp = { 'washer': [{ 'id': '', 'cur_temp': cur_temp }] }
 client.publish(out_oven_door_stat, json.dumps(payload_door))
 client.publish(out_oven_cur_stat, json.dumps(payload_cur))
-client.publish(out_oven_cur_temp, json.dumps(payload_temp))
+#client.publish(out_oven_cur_temp, json.dumps(payload_temp))
 
 print('rc: ' + str(rc)) 
